ed8e39438f7ccd287a035fa1f94b44456775e492
Your Name <saifi.mujeeb2@gmail.com>
2023-08-29 11:31:24 +0530

    Added: to demo rest cotroller

--- src/main/java/com/example/employeepayrollapp/controller/Controller.java
+++ src/main/java/com/example/employeepayrollapp/controller/Controller.java
@@ -1,7 +1,14 @@
 package com.example.employeepayrollapp.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+
+    @GetMapping("/get")
+    public String displayMessage(){
+        return "Hello World!";
+    }
+
 }
