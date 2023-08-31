ac52335fe4b075f224222f26ca0545684e593888
Your Name <saifi.mujeeb2@gmail.com>
2023-08-31 09:29:29 +0530

    Added: using lombok library for logging levels

--- src/main/java/com/example/employeepayrollapp/EmployeepayrollappApplication.java
+++ src/main/java/com/example/employeepayrollapp/EmployeepayrollappApplication.java
@@ -1,13 +1,16 @@
 package com.example.employeepayrollapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeepayrollappApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EmployeepayrollappApplication.class, args);
+        log.info("Welcome to Employee Payroll App");
     }
 
 }
