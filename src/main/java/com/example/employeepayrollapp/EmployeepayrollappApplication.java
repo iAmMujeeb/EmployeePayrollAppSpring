3846594c69d1eb64402b7d0eac9332adeb823702
Your Name <saifi.mujeeb2@gmail.com>
2023-08-31 08:57:56 +0530

    Added: using lombok library for getters and setters

--- src/main/java/com/example/employeepayrollapp/EmployeepayrollappApplication.java
+++ src/main/java/com/example/employeepayrollapp/EmployeepayrollappApplication.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmployeepayrollappApplication {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Payroll App");
-		SpringApplication.run(EmployeepayrollappApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeepayrollappApplication.class, args);
+    }
 
 }
